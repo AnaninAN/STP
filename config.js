56b7bcf66151ae7f3966d1187ec6ced45b3fb063
AndreyOsipuk <45951478+AndreyOsipuk@users.noreply.github.com>
2019-10-23 01:33:36 +0400

    start webpack

--- config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    port: 3000,
-    mongoURI: 'mongodb+srv://admin:admin@cluster0-6r1ny.gcp.mongodb.net/stp_test_db?retryWrites=true',
-};
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,13 @@
+
+<template src="./HelloWorld.html"></template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped lang="scss" src="./HelloWorld.scss"></style>
