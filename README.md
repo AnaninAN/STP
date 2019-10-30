7b4dc4df1d33503c981e313ff61b51fa1ca182bf
AnaninAN <ananinpgu@mail.ru>
2019-10-31 00:53:22 +0300

    start of the first page

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# vue-stream
+# stp
--- src/router.js
+++ src/router.js
@@ -3,7 +3,9 @@ import VueRouter from 'vue-router';
 
 Vue.use(VueRouter);
 
-const PageHome = () => import(/* webpack-chunk-name: "PageHome" */ './components/pages/PageHome.vue');
+const PageHome = () => import(/* webpack-chunk-name: "PageHome" */ './components/pages/PageHome.vue'),
+  PageMyRoutes = () => import(/* webpack-chunk-name: "PageMyRoutes" */ './components/pages/PageMyRoutes.vue'),
+  PageLogin = () => import(/* webpack-chunk-name: "PageLogin" */ './components/pages/PageLogin.vue');
 
 export default new VueRouter({
   routes: [
@@ -11,6 +13,16 @@ export default new VueRouter({
       name: 'home',
       path: '/',
       component: PageHome,
+    },
+    {
+      name: 'my-routes',
+      path: '/myroutes',
+      component: PageMyRoutes,
+    },
+    {
+      name: 'login',
+      path: '/login',
+      component: PageLogin,
     },
   ],
   mode: 'history',
