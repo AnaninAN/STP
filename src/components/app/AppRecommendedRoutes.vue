8ce246856edaabc2e710b37355042971ea99461e
AnaninAN <ananinpgu@mail.ru>
2019-11-02 18:40:24 +0300

    layout of component AppRecommendedRoutes.vue

---
+++ src/components/app/AppRecommendedRoutes.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="recommended_routes">
+    <div class="container">
+      <p class="recommended_routes__title">Мы приготовили лучшие маршруты для Вас</p>
+    </div>
+    <div class="container justify-content-around">
+      <div class="row">
+        <div class="col-md-4">
+          <a href="" class="square"></a>
+        </div>
+        <div class="col-md-4">
+          <a href="" class="square"></a>
+        </div>
+        <div class="col-md-4">
+          <a href="" class="square"></a>
+        </div>
+        <div class="col-md-4">
+          <a href="" class="square"></a>
+        </div>
+        <div class="col-md-auto">
+          <a href="" class="rectangle"></a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppRecommendedRoutes',
+}
+</script>
+
+<style lang="scss" scoped>
+  .recommended_routes {
+    padding: 50px 0;
+    .recommended_routes__title {
+      text-align: center;
+      font-size: 24px;
+      margin-bottom: 0;
+      margin-bottom: 30px;
+    }
+  }
+  .square,
+  .rectangle {
+    display: block;
+    text-align: center;
+    line-height: 350px;
+    height: 350px;
+    background-color: #C4C4C4;
+  }
+  .square {
+    width: 350px;
+  }
+  .rectangle {
+    width: 730px;
+  }
+  .row div:nth-child(n+4) {
+    margin-top: 30px;
+  }
+</style>
--- src/components/pages/PageHome.vue
+++ src/components/pages/PageHome.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
     <app-route-building-form></app-route-building-form>
+    <app-recommended-routes></app-recommended-routes>
   </div>
 </template>
 
 <script>
 import AppRouteBuildingForm from '../app/AppRouteBuildingForm.vue';
+import AppRecommendedRoutes from '../app/AppRecommendedRoutes.vue';
 
 export default {
   name: 'PageHome',
   components: {
     AppRouteBuildingForm,
+    AppRecommendedRoutes,
   }
 }
 </script>
