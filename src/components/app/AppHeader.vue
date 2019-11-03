c7dbfba6f68b4b9002228e3a755c12e315d9b864
AnaninAN <ananinpgu@mail.ru>
2019-11-03 15:52:34 +0300

    added to the Home menu

--- src/components/app/AppHeader.vue
+++ src/components/app/AppHeader.vue
@@ -2,12 +2,17 @@
   <div class="header">
     <div class="container">
       <div class="row justify-content-between">
+        <div class="col-md-1">
+          <router-link :to="{ name: 'home' }" class="home">
+            Главная
+          </router-link>
+        </div>
         <div class="col-md-2">
           <router-link :to="{ name: 'my-routes' }" class="my-routes">
             Мои маршруты
           </router-link>
         </div>
-        <div class="col-md-2">
+        <div class="col-md-1">
           <router-link :to="{ name: 'login' }" class="login">
             Войти
           </router-link>
@@ -30,5 +35,9 @@ export default {
     padding: 50px 0;
     background: #F4F2F2;
     font-size: 14px;
+    a {
+      display: block;
+      text-align: center;
+    }
   }
 </style>
