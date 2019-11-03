5db593094649222c0795b5b7551cd165522ef673
AnaninAN <ananinpgu@mail.ru>
2019-11-03 15:53:33 +0300

    added a PageRouteSingle component to display a specific route

---
+++ src/components/pages/PageRouteSingle.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    <h1>Форма для route-single</h1>
+    <p>{{route}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PageRouteSingle',
+  props: ['id'],
+  data() {
+    return {
+      route: {},
+    }
+  },
+  mounted() {
+    fetch(`http://localhost:8888/routes/${this.id}`)
+      .then((response) => response.json())
+      .then((route) => {
+        this.route = route;
+      })
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
