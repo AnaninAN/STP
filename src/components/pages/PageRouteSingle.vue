3f09da64967e2b8d1fc84432080ce30748449c49
AnaninAN <ananinpgu@mail.ru>
2019-11-13 19:04:03 +0300

    Output information to the route component

--- src/components/pages/PageRouteSingle.vue
+++ src/components/pages/PageRouteSingle.vue
@@ -1,103 +1,93 @@
 <template>
   <div class="container">
     <div class="row">
-        <div class="table col-3 mt-4">
-            <div class="test__table">
-                <table>
-                <tr>
-                    <td class="test__table_td test__table_td_indent" rowspan="4"></td>
-                    <td class="test__table_td test__table_td_circle" colspan="2">10:00</td>
-                    <td class="test__table_td test__table_td_text" colspan="2" rowspan="4">kafnjsdnfjjansd
-                        sdfaskdjf
-                    </td>
-                </tr>
-                <tr>
-                    <td class="test__table_td test__table_td_r"></td>
-                    <td class="test__table_td"></td>
+      <p>{{route}}</p>
+    </div>
+    <div class="row">
+      <div class="table col-3 mt-4">
+        <div class="test__table">
+          <table>
+            <tr>
+              <td class="test__table_td test__table_td_indent" rowspan="4"></td>
+              <td class="test__table_td test__table_td_circle" colspan="2">10:00</td>
+              <td class="test__table_td test__table_td_text" colspan="2" rowspan="4">kafnjsdnfjjansd
+                  sdfaskdjf
+              </td>
+            </tr>
+            <tr>
+              <td class="test__table_td test__table_td_r"></td>
+              <td class="test__table_td"></td>
 
-                </tr>
-                <tr>
-                    <td class="test__table_td test__table_td_text" colspan="2">15 Мин</td>
-                </tr>
-                <tr>
-                    <td class="test__table_td test__table_td_r"></td>
-                    <td class="test__table_td"></td>
-                </tr>
-                <tr>
-                    <td class="test__table_td test__table_td_indent" rowspan="4"></td>
-                    <td class="test__table_td test__table_td_circle" colspan="2">11:15 - 12:15</td>
-                    <td class="test__table_td test__table_td_text" colspan="2" rowspan="4">kafnjsdnfjjansd
-                        sdfaskdjf adgosjdkzmfoian
-                    </td>
-                </tr>
-                <tr>
-                    <td class="test__table_td test__table_td_r"></td>
-                    <td class="test__table_td"></td>
+            </tr>
+            <tr>
+              <td class="test__table_td test__table_td_text" colspan="2">15 Мин</td>
+            </tr>
+            <tr>
+              <td class="test__table_td test__table_td_r"></td>
+              <td class="test__table_td"></td>
+            </tr>
+            <tr>
+              <td class="test__table_td test__table_td_indent" rowspan="4"></td>
+              <td class="test__table_td test__table_td_circle" colspan="2">11:15 - 12:15</td>
+              <td class="test__table_td test__table_td_text" colspan="2" rowspan="4">kafnjsdnfjjansd
+                  sdfaskdjf adgosjdkzmfoian
+              </td>
+            </tr>
+            <tr>
+              <td class="test__table_td test__table_td_r"></td>
+              <td class="test__table_td"></td>
 
-                </tr>
-                <tr>
-                    <td class="test__table_td test__table_td_text" colspan="2">15 Мин</td>
-                </tr>
-                <tr>
-                    <td class="test__table_td test__table_td_r"></td>
-                    <td class="test__table_td"></td>
-                </tr>
-                <tr>
-                    <td class="test__table_td test__table_td_indent" rowspan="4"></td>
-                    <td class="test__table_td test__table_td_circle" colspan="2">12:30 - 13:30</td>
-                    <td class="test__table_td test__table_td_text" colspan="2" rowspan="4">kafnjsdnfjjansd
-                        sdfaskdjf
-                    </td>
-                </tr>
-                <tr>
-                    <td class="test__table_td test__table_td_r"></td>
-                    <td class="test__table_td"></td>
-
-                </tr>
-                <tr>
-                    <td class="test__table_td test__table_td_text" colspan="2">35 Мин</td>
-                </tr>
-                <tr>
-                    <td class="test__table_td test__table_td_r"></td>
-                    <td class="test__table_td"></td>
-                </tr>
-                <tr>
-                    <td class="test__table_td test__table_td_indent" rowspan="4"></td>
-                    <td class="test__table_td test__table_td_circle" colspan="2">14:00 - 14:35</td>
-                    <td class="test__table_td test__table_td_text" colspan="2" rowspan="4">kafnjsdnfjjansd
-                        sdfaskdjf adgosjdkzmfoian
-                    </td>
-                </tr>
-                </table>
-            </div>
+            </tr>
+            <tr>
+              <td class="test__table_td test__table_td_text" colspan="2">15 Мин</td>
+            </tr>
+            <tr>
+              <td class="test__table_td test__table_td_r"></td>
+              <td class="test__table_td"></td>
+            </tr>
+            <tr>
+              <td class="test__table_td test__table_td_indent" rowspan="4"></td>
+              <td class="test__table_td test__table_td_circle" colspan="2">12:30 - 13:30</td>
+              <td class="test__table_td test__table_td_text" colspan="2" rowspan="4">kafnjsdnfjjansd
+                  sdfaskdjf
+              </td>
+            </tr>
+            <tr>
+              <td class="test__table_td test__table_td_r"></td>
+              <td class="test__table_td"></td>
+            </tr>
+            <tr>
+              <td class="test__table_td test__table_td_text" colspan="2">35 Мин</td>
+            </tr>
+            <tr>
+              <td class="test__table_td test__table_td_r"></td>
+              <td class="test__table_td"></td>
+            </tr>
+            <tr>
+              <td class="test__table_td test__table_td_indent" rowspan="4"></td>
+              <td class="test__table_td test__table_td_circle" colspan="2">14:00 - 14:35</td>
+              <td class="test__table_td test__table_td_text" colspan="2" rowspan="4">kafnjsdnfjjansd
+                  sdfaskdjf adgosjdkzmfoian
+              </td>
+            </tr>
+          </table>
         </div>
+      </div>
 
-        <div class="col-6 mt-4">
-            <div id="yandexmap"></div>
-        </div>
-        <div class="col-3 mt-4">
-            <h2>Hotel de Ville</h2>
-            <div>4.8 &#9733;</div>
-            <h6>Административное здание</h6>
-            <img src="..." class="img-rounded m-x-auto d-block" alt="...">
-            <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A assumenda commodi corporis cumque distinctio dolorem dolores doloribus, dolorum ducimus enim error est ex hic impedit incidunt ipsa ipsam numquam obcaecati quam quasi quisquam similique, sit, tempora tenetur ullam veniam voluptatem.</div><br>
-            <div>1200 &#8381;</div><br>
-            <div>5 туров с Hotel de Ville</div>
-        </div>
+      <div class="col-6 mt-4">
+        <div id="yandexmap"></div>
+      </div>
+      <div class="col-3 mt-4">
+        <h2>Hotel de Ville</h2>
+        <div>4.8 &#9733;</div>
+        <h6>Административное здание</h6>
+        <img src="" class="img-rounded m-x-auto d-block" alt="">
+        <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A assumenda commodi corporis cumque distinctio dolorem dolores doloribus, dolorum ducimus enim error est ex hic impedit incidunt ipsa ipsam numquam obcaecati quam quasi quisquam similique, sit, tempora tenetur ullam veniam voluptatem.</div><br>
+        <div>1200 &#8381;</div><br>
+        <div>5 туров с Hotel de Ville</div>
+      </div>
     </div>
 </div>
-<script>
-ymaps.ready(function(){
-
-    var yMap = new ymaps.Map("yandexmap", {
-        center: [57.145, 65.523],
-        zoom: 14
-    });
-
-        yMap.geoObjects
-            .add(point);
-});
-</script>
 </template>
 
 <script>
