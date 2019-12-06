d05192c5e6da459bf09f43e8d998e541906e3ab8
AnaninAN <ananinpgu@mail.ru>
2019-12-06 20:14:43 +0300

    start develop_react

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import 'assets/main.scss';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { HomePage } from 'components/HomePage'
+
+ReactDOM.render(
+  <HomePage />,
+  document.getElementById('root')
+);
