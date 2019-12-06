d9107e8944fb1e167256a1f72bb0c664f1123ecf
AnaninAN <ananinpgu@mail.ru>
2019-12-06 19:49:50 +0300

    !

--- server/config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    mongoURI: 'mongodb+srv://admin:admin@cluster0-6r1ny.gcp.mongodb.net/smart_travelling_db',
-    //mongoURI: 'mongodb://localhost:27017/test',
-    port: 8888,
-};
--- server/routes/index.js
+++
@@ -1,7 +0,0 @@
-const routerUser = require('./user');
-const routerRoute = require('./route');
-
-module.exports = {
-  routerUser,
-  routerRoute
-};
